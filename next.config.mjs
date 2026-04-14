8d404167f7fe6f308bfbb33f3870390c934ef392
Ahmed Mustafa <ahmed@example.com>
2026-04-14 06:14:24 +0200

    sdfvg

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com','pixels.com'],
+    domains: ['images.pexels.com', 'pixels.com'],
   },
-  // السطرين دول هما اللي هيحلوا المشكلة
-  webpack: (config, { isServer }) => {
-    config.optimization.splitChunks = false;
+  // السطر ده هو السر
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 20000000, // بيجبره يخلي أقصى حجم لأي ملف 20 ميجا
+    };
     return config;
   },
-  productionBrowserSourceMaps: false,
 }
 
 export default nextConfig
