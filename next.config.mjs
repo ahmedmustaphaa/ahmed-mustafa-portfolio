82fe8d937e100d0b094b2136f72e0688b63ee06b
Ahmed Mustafa <ahmed@example.com>
2026-04-14 06:08:31 +0200

    cvdgb

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com','pixels.com'],
   },
+  // السطرين دول هما اللي هيحلوا المشكلة
+  webpack: (config, { isServer }) => {
+    config.optimization.splitChunks = false;
+    return config;
+  },
+  productionBrowserSourceMaps: false,
 }
 
 export default nextConfig
